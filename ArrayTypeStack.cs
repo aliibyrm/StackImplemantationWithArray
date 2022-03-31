ae4a113678247c1861f2373d48b0b3ac3a145df0
Ali Bayram <81009538+aliibyrm@users.noreply.github.com>
2022-03-31 20:20:03 +0300

    Add files via upload

---
+++ ArrayTypeStack.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class ArrayTypeStack : IStack
+    {
+        private object [] items;
+        private int top = -1;
+
+        public int Top {
+            get
+            {
+                return top;
+            }
+            set
+            {
+                top = value;
+            }
+
+
+
+        }
+
+        public ArrayTypeStack()
+        {
+      
+        }
+
+        public ArrayTypeStack(int itemCount)
+        {
+            this.items = new object[itemCount];
+        }
+
+        public int GetTop()
+        {
+            return top;
+
+        }
+
+        public void SetTop(int value)
+        {
+            top = value;
+        }
+
+        public bool IsEmpty()
+        {
+            return Top == -1 ? true : false;
+        }
+
+        public object Peek()
+        {
+           return items [Top];
+        }
+
+        public object Pop()
+        {
+            if (IsEmpty())
+            {
+
+                throw new Exception("Hata: Stack Boş...(Owerflow)");
+            }
+            Object temp = items[top--];
+            return temp;
+        }
+
+        public void Push(object item)
+        {
+            if(items.Length==top+1)
+            {
+                throw new Exception("Hata: Stack Dolu...(Owerflow)");
+            }
+            items[++top] = item;
+            
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void arrayToStackbtn_Click(object sender, EventArgs e)
+        {
+            ArrayTypeStack ats = new ArrayTypeStack(8);
+            ats.Push(1);
+            ats.Push(2);
+            ats.Push(3);
+            
+            MessageBox.Show(ats.Pop().ToString());
+            MessageBox.Show(ats.Pop().ToString());
+            MessageBox.Show(ats.Peek().ToString());
+
+            string stack = ats.Pop().ToString();
+            MessageBox.Show(stack);
+
+        }
+    }
+}
